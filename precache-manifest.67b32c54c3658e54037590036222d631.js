5c49fc55e474a8c8cd6043be1c076f704618ae83
Brahmeshwar Sriniketh <brahmeshwar@convertcart.com>
2020-02-20 12:52:10 +0530

    Updates

---
+++ precache-manifest.67b32c54c3658e54037590036222d631.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de46afc8592940043a68ee4ee8840c5",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "50f2580cfd3d6361bea5",
+    "url": "/resume/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d57292e9abf1a77a6723",
+    "url": "/resume/static/js/2.e589bb0f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/resume/static/js/2.e589bb0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50f2580cfd3d6361bea5",
+    "url": "/resume/static/js/main.27cbdd5f.chunk.js"
+  },
+  {
+    "revision": "540de6763af4833a1da8",
+    "url": "/resume/static/js/runtime-main.64f02ed5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
